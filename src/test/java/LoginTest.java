cae399df9076b2470a1c82ef68d29aa00e71b8d7
Nageswara, Eslavath <nageswara.eslavath@gmail.com>
2024-12-07 14:56:51 +0530

    logintest reverted dashboard123

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,7 +9,7 @@ public class LoginTest extends BaseTest {
    @Test(testName = "Verify the HRM Login page with Valid Username and Password",description = "Login feature",priority = 1
            ,groups = "smoke test")
     public void loginApp() throws Exception{
-       String exceptedValue = "Dashboard123";
+       String exceptedValue = "Dashboard";
        LoginPage loginPage = new LoginPage(page);
        loginPage.loadingHRMLoginPage();
        String acutalValue = loginPage.loginHRM("Admin","admin123");
